15a4715442087cf104bd7e627330b5bb25c94652
Iris <iris@raidev.eu>
2026-01-28 17:50:40 +0200

    cool bash script

--- gallery/gallery-add.sh
+++ gallery/gallery-add.sh
@@ -2,7 +2,16 @@
 database=~/Documents/rai-fancy-site/gallery/art-database.json
 art_folder=~/Documents/rai-fancy-site/media/Artwork
 
-npx -y avif --input $1 --output $art_folder
-
-#sed -i -e 's/        }\n    ]\n}/big/g' $database
-
+filename=$(basename $1)
+date=$(date -r $1 "+%Y-%b-%d")
+cp $1 $art_folder
+cd $art_folder
+npx -y avif
+rm $filename
+wl-copy ',
+		{"filename":"'${filename/.png/}'",
+		"title":"'${filename/.png/}'",
+		"date":"$date",
+		"tags": []
+		}'
+wl-paste
