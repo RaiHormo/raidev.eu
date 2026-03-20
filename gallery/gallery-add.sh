5bec7e0be925ce6cc50e87925547b4c143a06139
Iris <iris@raidev.eu>
2026-03-20 22:35:06 +0200

    fixed add script mistake

--- gallery/gallery-add.sh
+++ gallery/gallery-add.sh
@@ -2,12 +2,12 @@
 database=~/Documents/rai-fancy-site/gallery/art-database.json
 art_folder=~/Documents/rai-fancy-site/media/Artwork
 
-filename=$(basename $1)
-date=$(date -r $1 "+%Y-%b-%d")
-cp $1 $art_folder
-cd $art_folder
+filename=$(basename "$1")
+date=$(date -r "$1" "+%Y-%b-%d")
+cp "$1" "$art_folder"
+cd "$art_folder"
 npx -y avif
-rm $filename
+rm "$filename"
 wl-copy ',
 		{"filename":"'${filename/.png/}'",
 		"title":"'${filename/.png/}'",
