c9eead305fc2a7291a5f0084ff7505a47500b95e
RaiHormo <iris@raidev.eu>
2026-06-06 04:58:02 +0300

    added and improved to the gallery

--- gallery/gallery-add.sh
+++ gallery/gallery-add.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-database=~/Documents/rai-fancy-site/gallery/art-database.json
-art_folder=~/Documents/rai-fancy-site/media/Artwork
+database=./art-database.json
+art_folder=../media/Artwork
 
 filename=$(basename "$1")
 date=$(date -r "$1" "+%Y-%b-%d")
 cp "$1" "$art_folder"
 cd "$art_folder"
-npx -y avif
+avif
 rm "$filename"
 wl-copy ',
 		{"filename":"'${filename/.png/}'",
